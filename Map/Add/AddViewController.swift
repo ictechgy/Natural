4a9eab63a63d019db04ab55758a974c3d92b5f3b
ictechgy <ictechgy@gmail.com>
2021-03-17 23:51:22 +0900

    데이터 추가 화면 구현 중

--- Map/Add/AddViewController.swift
+++ Map/Add/AddViewController.swift
@@ -11,11 +11,49 @@ import RxSwift
 class AddViewController: UIViewController {
     
     var viewModel: AddViewModel!
+    var disposeBag = DisposeBag()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var roadAddressField: UITextField!
+    @IBOutlet weak var numberAddressField: UITextField!
+    @IBOutlet weak var detailAddressField: UITextField!
+    @IBOutlet weak var characterField: UITextField!
+    @IBOutlet weak var manageField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        typeField.rx.text.orEmpty
+            .bind(to: viewModel.typeRelay)
+            .disposed(by: disposeBag)
+        
+        roadAddressField.rx.text.orEmpty
+            .bind(to: viewModel.roadAddressRelay)
+            .disposed(by: disposeBag)
+        
+        numberAddressField.rx.text.orEmpty
+            .bind(to: viewModel.numberAddressRelay)
+            .disposed(by: disposeBag)
+        
+        detailAddressField.rx.text.orEmpty
+            .bind(to: viewModel.detailAddressRelay)
+            .disposed(by: disposeBag)
+        
+        characterField.rx.text.orEmpty
+            .bind(to: viewModel.characterRelay)
+            .disposed(by: disposeBag)
+        
+        manageField.rx.text.orEmpty
+            .bind(to: viewModel.manageRelay)
+            .disposed(by: disposeBag)
+        
+        viewModel.addButtonEnabled
+            .bind(to: addButton.rx.isEnabled)
+            .disposed(by: disposeBag)
     }
     
 
--- Map/Add/AddViewModel.swift
+++ Map/Add/AddViewModel.swift
@@ -12,7 +12,21 @@ import NMapsMap
 
 class AddViewModel {
     
+    
+    var typeRelay: BehaviorRelay = BehaviorRelay(value: "")
+    var roadAddressRelay: BehaviorRelay = BehaviorRelay(value: "")
+    var numberAddressRelay: BehaviorRelay = BehaviorRelay(value: "")
+    var detailAddressRelay: BehaviorRelay = BehaviorRelay(value: "")
+    var characterRelay: BehaviorRelay = BehaviorRelay(value: "")
+    var manageRelay: BehaviorRelay = BehaviorRelay(value: "")
+    
+    var addButtonEnabled: BehaviorRelay = BehaviorRelay(value: false)
+    
+    var disposeBag = DisposeBag()
+    
     init(position: NMGLatLng) {
+        
+        //position을 가지고 주소 값 가져오기
         
     }
     
